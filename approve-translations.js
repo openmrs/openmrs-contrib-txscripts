8934eaf69920a20cafbbafcb4665edf38abf2c8e
Darius Jazayeri <jazayeri@alum.mit.edu>
2017-02-04 08:47:26 -0800

    Refactor to use modern JS

--- approve-translations.js
+++ approve-translations.js
@@ -1,80 +1,145 @@
 require('dotenv').config();
-var rp = require('request-promise');
-var _ = require("lodash");
-var md5 = require("md5");
+const fetch = require('isomorphic-fetch');
+const md5 = require('md5');
+const base64 = require('base-64');
 
-var TRANSIFEX_BASE = "https://www.transifex.com/api/2/";
+const TRANSIFEX_BASE = "https://www.transifex.com/api/2/";
 
-var SKIP = ["openmrs-core"];
+const EXCLUDE_RESOURCES = ["openmrs-core"];
 
-var auth = {
-    user: process.env.TX_USERNAME,
-    pass: process.env.TX_PASSWORD
+const IS_DEBUG = process.env.NODE_ENV !== 'production';
+
+let totalMarkedAsReviewed = 0;
+let totalAlreadyReviewed = 0;
+
+const fetchOpts = {
+    headers: new Headers({
+        'Authorization': `Basic ${base64.encode(process.env.TX_USERNAME + ':' + process.env.TX_PASSWORD)}`,
+        'Accept': 'application/json'
+    })
 };
-var opts = {
-    json: true,
-    auth: auth
-};
 
-var alreadyReviewed = 0;
+const putOpts = {
+    method: 'PUT',
+    headers: new Headers({
+        'Authorization': `Basic ${base64.encode(process.env.TX_USERNAME + ':' + process.env.TX_PASSWORD)}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    })
+}
 
-rp(_.merge({
-               uri: TRANSIFEX_BASE + "project/OpenMRS",
-               qs: {
-                   details: "true"
-               }
-           }, opts)).then(function (allProjects) {
-    var resources = _.filter(allProjects.resources, function (val) {
-        return _.indexOf(SKIP, val.slug) < 0
-    });
-    console.log("There are " + resources.length + " resources");
+function jsonResponse(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response.json();
+    }
+    else {
+        throw {message: `${response.url} => ${response.status} ${response.statusText}`};
+    }
+}
 
-    resources.forEach(function (resource) {
-        console.log(resource.slug);
+function logObject(object) {
+    console.log(object);
+    return object;
+}
 
-        rp(_.merge({
-                       uri: TRANSIFEX_BASE + "project/OpenMRS/resource/" + resource.slug,
-                       qs: {
-                           details: "true"
-                       }
-                   }, opts)).then(function (resource) {
-            console.log("languages: " + resource.available_languages.length);
-            var languageCodes = _.map(resource.available_languages, "code");
+function handleError(error) {
+    console.log("ERROR!");
+    console.log(error.message ? error.message : error);
+}
 
-            languageCodes.forEach(function (languageCode) {
-                if (languageCode == resource.source_language_code) {
-                    return;
-                }
-                rp(_.merge({
-                               uri: TRANSIFEX_BASE + "project/OpenMRS/resource/" + resource.slug + "/translation/" + languageCode + "/strings"
-                           }, opts)).then(function (allTranslations) {
-                    var needsReview = allTranslations.filter(function (it) {
-                        return it.translation && !it.reviewed;
-                    });
-                    allTranslations.forEach(function (it) {
-                        if (it.reviewed) {
-                            alreadyReviewed += 1;
-                            console.log("Already reviewed: " + alreadyReviewed);
-                        }
-                    })
-                    needsReview.forEach(function (it) {
-                        var hash = md5([it.key, ""].join(":"));
-                        rp({
-                               method: "PUT",
-                               uri: TRANSIFEX_BASE + "project/OpenMRS/resource/" + resource.slug + "/translation/" + languageCode + "/string/" + hash + "/",
-                               auth: auth,
-                               json: {
-                                   translation: it.translation,
-                                   reviewed: true
-                               }
-                           }).then(function (response) {
-                            console.log("Marked as reviewed: " + resource.slug + " " + languageCode + " " + it.key + " = " + it.translation);
-                        })
-                    });
-                })
-            });
+function curryHandleError(forWhat) {
+    return function handleError(error) {
+        console.log("---ERROR FOR------------------");
+        console.log(forWhat);
+        console.log("------------------------------");
+        console.log(error.message ? error.message : error);
+        console.log("------------------------------");
+    }
+}
+
+function curryHandleLanguage(resource, languageCode) {
+    return function (allTranslations) {
+        const needsReview = allTranslations.filter(it => it.translation && !it.reviewed);
+        const alreadyReviewed = allTranslations.length - needsReview.length;
+        totalAlreadyReviewed += alreadyReviewed;
+        console.log(`${resource.slug} ${languageCode} has ${needsReview.length} to review (and ${alreadyReviewed} already reviewed)`);
+        const promises = [];
+        needsReview.forEach(it => {
+            const useOpts = Object.assign({
+                                              body: JSON.stringify({
+                                                                       translation: it.translation,
+                                                                       reviewed: true
+                                                                   })
+                                          }, putOpts);
+            const hash = md5([it.key, ""].join(":"));
+            const uri = `${TRANSIFEX_BASE}project/OpenMRS/resource/${resource.slug}/translation/${languageCode}/string/${hash}/`;
+            promises.push(
+                    fetch(uri, useOpts)
+                            .then(response => {
+                                console.log(`Marked as reviewed: ${resource.slug} ${languageCode} ${it.key}=${it.translation}`);
+                                totalMarkedAsReviewed += 1;
+                            })
+                            .catch(curryHandleError("PUT " + uri))
+            );
         });
+        return Promise.all(promises);
+    }
+}
+
+function handleResource(resource) {
+    if (IS_DEBUG) {
+        console.log(`${resource.slug} has ${resource.available_languages.length} languages`);
+    }
+
+    const languageCodes = resource.available_languages.map(l => l.code);
+
+    const promises = [];
+    languageCodes.forEach(languageCode => {
+        if (languageCode == resource.source_language_code) {
+            if (IS_DEBUG) {
+                console.log(`Skipping source language ${languageCode}`);
+            }
+            return;
+        }
+        const handleLanguage = curryHandleLanguage(resource, languageCode);
+        const uri = `${TRANSIFEX_BASE}project/OpenMRS/resource/${resource.slug}/translation/${languageCode}/strings`;
+        promises.push(
+                fetch(uri, fetchOpts)
+                        .then(jsonResponse)
+                        .then(handleLanguage)
+                        .catch(curryHandleError(uri))
+        );
     });
-}).catch(function (err) {
-    console.log(err.message);
-});
+    return Promise.all(promises);
+}
+
+function handleProject(project) {
+    const resources = project.resources.filter(val => EXCLUDE_RESOURCES.indexOf(val.slug) < 0);
+    console.log(`Will handle ${resources.length} resources`);
+
+    const promises = [];
+    resources.forEach(resource => {
+        if (IS_DEBUG) {
+            console.log(`Handling ${resource.slug}`);
+        }
+
+        promises.push(
+                fetch(`${TRANSIFEX_BASE}project/OpenMRS/resource/${resource.slug}?details=true`, fetchOpts)
+                        .then(jsonResponse)
+                        .then(handleResource)
+                        .catch(curryHandleError(resource.slug))
+        );
+    });
+    return Promise.all(promises);
+}
+
+fetch(`${TRANSIFEX_BASE}project/OpenMRS?details=true`, fetchOpts)
+        .then(jsonResponse)
+        .then(handleProject)
+        .then(() => {
+            console.log("DONE!");
+            console.log("Reviewed: " + totalMarkedAsReviewed);
+            console.log("(ignored " + totalAlreadyReviewed + " that were already reviewed");
+            return "DONE";
+        })
+        .catch(handleError);
